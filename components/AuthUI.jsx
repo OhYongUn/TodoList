af546422cc0240e5b9545f455a6de1f52cea896c
OhYongUn <dhdyddjs@naver.com>
2024-06-23 16:27:42 +0900

    update AuthHeader,AuthUi

--- components/AuthUI.jsx
+++ components/AuthUI.jsx
@@ -6,29 +6,46 @@ import { createSupabaseBrowserClient } from "@/lib/client/supabase";
 import useHydrate from "@/hooks/useHydrate";
 
 const AuthUI = () => {
-  const supabase = createSupabaseBrowserClient();
   const [user, setUser] = useState();
-
+  const supabase = createSupabaseBrowserClient();
   const isMount = useHydrate();
-  const getUserInfo = async () => {
-    console.log("getUserInfo");
+
+  const getUserInfo = useCallback(async () => {
     const result = await supabase.auth.getUser();
-    console.log("result", result);
     if (result?.data?.user) setUser(result?.data?.user);
-  };
+  }, [supabase]);
+
   const handleLogout = async () => {
     await supabase.auth.signOut();
     window.location.reload();
   };
+
+  const handleGoogleLogin = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: process.env.NEXT_PUBLIC_AUTH_REDIRECT_TO,
+      },
+    });
+  };
+
+  const handleGithubLogin = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "github",
+      options: {
+        redirectTo: process.env.NEXT_PUBLIC_AUTH_REDIRECT_TO,
+      },
+    });
+  };
+
   useEffect(() => {
     getUserInfo();
-  }, []);
+  }, [getUserInfo]);
 
-  if (!isMount) {
-    return null;
-  }
+  if (!isMount) return null;
+
   return (
-    <section className="w-full">
+    <section className="w-full p-10 ">
       <div> {user ? `로그인 됨 ${user?.email}` : "로그아웃"}</div>
       <>
         {user && (
@@ -37,7 +54,7 @@ const AuthUI = () => {
           </button>
         )}
       </>
-      <div className="mx-auto max-w-[500px]">
+      <div className=" mx-auto max-w-[500px]">
         <Auth
           redirectTo={process.env.NEXT_PUBLIC_AUTH_REDIRECT_TO}
           supabaseClient={supabase}
